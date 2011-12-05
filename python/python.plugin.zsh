6781478d270120a60c3648ab0c9f665e2998dafa
Alfredo Deza <alfredodeza@gmail.com>
2011-12-05 09:09:59 -0500

    add a try function and better up cdp with try/except

--- python/python.plugin.zsh
+++ python/python.plugin.zsh
@@ -1,9 +1,19 @@
 # remove pyc files
 alias pyclean='find . -type f -name "*.py[co]" -exec rm -f \{\} \;'
 
+# Does my python module exist?
+try() {
+    python -c "exec 'try: import ${1} as _; print _.__file__\nexcept Exception, e: print e'"
+}
+
 # change to python package directory
 cdp() {
-    cd $(python -c "import os.path as _, ${1}; print _.dirname(_.realpath(${1}.__file__))")
+    MODULE_DIRECTORY=`python -c "exec 'try: import os.path as _, ${1}; print _.dirname(_realpath(${1}.__file__))\nexcept Exception: pass'"`
+    if  [[ -d $MODULE_DIRECTORY ]]; then
+        cd $MODULE_DIRECTORY
+    else
+        echo "Module ${1} was not found or is not importable"
+    fi
 }
 
 walkup() {
