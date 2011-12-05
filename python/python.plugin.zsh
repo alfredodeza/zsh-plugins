db42fec5d53bca0e3e8677adacc9311eb8413716
Alfredo Deza <alfredodeza@gmail.com>
2011-12-05 09:16:29 -0500

    fix syntax error, better error message

--- python/python.plugin.zsh
+++ python/python.plugin.zsh
@@ -8,11 +8,11 @@ try() {
 
 # change to python package directory
 cdp() {
-    MODULE_DIRECTORY=`python -c "exec 'try: import os.path as _, ${1}; print _.dirname(_realpath(${1}.__file__))\nexcept Exception: pass'"`
+    MODULE_DIRECTORY=`python -c "exec 'try: import os.path as _, ${1}; print _.dirname(_.realpath(${1}.__file__))\nexcept Exception, e: print e'"`
     if  [[ -d $MODULE_DIRECTORY ]]; then
         cd $MODULE_DIRECTORY
     else
-        echo "Module ${1} was not found or is not importable"
+        echo "Module ${1} was not found or is not importable: $MODULE_DIRECTORY"
     fi
 }
 
