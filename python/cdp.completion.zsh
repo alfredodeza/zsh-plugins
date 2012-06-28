d1de41722aa10fa460f1edc5d0bb292166995f96
Alfredo Deza <alfredodeza@gmail.com>
2012-06-28 09:08:18 -0400

    cdp completion

--- python/cdp.completion.zsh
+++ python/cdp.completion.zsh
@@ -1,14 +1,14 @@
 #compdef cdp
 
 _cdp_commands() {
-  # this function calls IPython and loops over the list of apps and options
-  # and constructs the help menu with it
+  # this function calls IPython and loops over the list of modules
+  # available in a given context. It is a bit slow and I have no idea
+  # how to make it faster rather than using cache which I can't figure out
 
   local line
   local -a cmdlist
   _call_program commands "python -c \"exec('from IPython.core.completerlib import module_completion\nfor i in module_completion(\'import \'): print i')\"" | while read -A line; do
      cmdlist=($cmdlist "${line[1]}")
-     #cmdlist=($cmdlist "${line[1]%,}:${line[2,-1]}")
    done
 
  _describe -t commands 'python modules' cmdlist && ret=0
@@ -16,16 +16,10 @@ _cdp_commands() {
 
 _cdp() {
   local curcontext=$curcontext ret=1
-  local curcontext=$curcontext ret=1
 
   if ((CURRENT == 2)); then
     _cdp_commands
   fi
-  #if [[ "$PREFIX" = * ]]; then
-    #_cdp_commands
-  #else
-    #_files
-  #fi
 }
 
 compdef _cdp cdp
